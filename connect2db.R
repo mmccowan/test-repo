cbce9f57373e688085c19c081d9bacde9bc0e916
mmccowan <mmccowan@qti.qualcomm.com>
2015-01-06 10:13:33 -0800

    adding to git index

---
+++ connect2db.R
@@ -0,0 +1,6 @@
+## Getting Data Overview
+
+ucscDb <- dbConnect(MySQL(), user = "genome", host = "genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucscDb, "show databases;")
+dbDisconnect(ucscDb)
+result
